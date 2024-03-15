2fce56d421a8ef9d030b51ca7f56b08e04361772
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-03-15 13:01:08 -0700

    first commit

---
+++ Lesson1/student.js
@@ -0,0 +1,15 @@
+exports.getName = () => {
+  return "Mahmudul Hasan";
+};
+const getAge = () => {
+  return "24";
+};
+const cgpa = "3.22";
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+// module.exports = {
+//   getName,
+//   getAge,
+//   cgpa,
+// };
